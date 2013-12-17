2b585f95b2069c2e16e2d38d400f357cb5833a54
daniffig <daniffig@gmail.com>
2013-12-17 19:44:27 -0300

    Le agregué una boludez al botón Configuración.

--- JBomb/src/com/example/jbomb/MainActivity.java
+++ JBomb/src/com/example/jbomb/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.jbomb;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button settingsButton = (Button) this.findViewById(R.id.settingsButton);
+        
+        settingsButton.setOnClickListener(new OnClickListener()
+        {
+        	public void onClick(View v)
+        	{
+                Toast.makeText(MainActivity.this, "Clicked on Button", Toast.LENGTH_LONG).show();
+        	}
+        });        
     }
 
 
