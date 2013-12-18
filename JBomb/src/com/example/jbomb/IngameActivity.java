c127da07c58f605ce58e2294c06bc7f95d144e0d
daniffig <daniffig@gmail.com>
2013-12-18 16:43:53 -0300

    Magias

---
+++ JBomb/src/com/example/jbomb/IngameActivity.java
@@ -0,0 +1,90 @@
+package com.example.jbomb;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.DragEvent;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.Intent;
+
+public class IngameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ingame);
+		
+		this.findViewById(R.id.ingameBombImage).setOnTouchListener(new TouchListener());
+		this.findViewById(R.id.ingamePlayerTopImage).setOnDragListener(new DragListener());
+		this.findViewById(R.id.ingamePlayerRightImage).setOnDragListener(new DragListener());
+		this.findViewById(R.id.ingamePlayerBottomImage).setOnDragListener(new DragListener());
+		this.findViewById(R.id.ingamePlayerLeftImage).setOnDragListener(new DragListener());
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.ingame, menu);
+		return true;
+	}
+
+	
+	public void openQuiz(View view)
+	{
+	}
+	
+	@SuppressLint("NewApi")
+	class TouchListener implements OnTouchListener
+	{
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			// TODO Auto-generated method stub
+			
+			switch (motionEvent.getAction())
+			{
+			case MotionEvent.ACTION_DOWN:
+				ClipData data = ClipData.newPlainText("", "");
+				DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+				view.startDrag(data, shadowBuilder, view, 0);
+				break;
+			}
+			
+			return true;
+		}
+	}
+	
+	class DragListener implements OnDragListener
+	{
+		@Override
+		public boolean onDrag(View v, DragEvent event) {	
+			
+			View ingameBomb = IngameActivity.this.findViewById(R.id.ingameBombImage);
+			View ingameImage = IngameActivity.this.findViewById(v.getId());
+						
+			switch (event.getAction())
+			{
+			case DragEvent.ACTION_DRAG_EXITED:
+				ingameBomb.setVisibility(View.VISIBLE);
+				break;
+
+			case DragEvent.ACTION_DROP:
+
+		    	Intent myIntent = new Intent(IngameActivity.this, QuizActivity.class);
+		    	
+		    	myIntent.putExtra("TARGET_PLAYER_NAME", ingameImage.getContentDescription());
+
+		    	IngameActivity.this.startActivity(myIntent);
+				break;
+			}
+		    
+			return true;
+		}		
+	}
+}
---
+++ JBomb/src/com/example/jbomb/QuizActivity.java
@@ -0,0 +1,44 @@
+package com.example.jbomb;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+
+public class QuizActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quiz);
+		TextView tv = (TextView)findViewById(R.id.quizNotice);  
+		tv.setText("Debes responder la siguiente pregunta para pasar la bomba a " + this.getIntent().getExtras().getString("TARGET_PLAYER_NAME"));
+		
+		
+		// Show the Up button in the action bar.
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.quiz, menu);
+		return true;
+	}
+	
+	public void openGameOver(View view)
+	{
+		RadioGroup answersRadioGroup = (RadioGroup) findViewById(R.id.answersRadioGroup);
+		
+    	Intent myIntent = new Intent(QuizActivity.this, GameOverActivity.class);
+    	myIntent.putExtra("answerID", answersRadioGroup.getCheckedRadioButtonId());
+    	QuizActivity.this.startActivity(myIntent);
+	}
+
+}
