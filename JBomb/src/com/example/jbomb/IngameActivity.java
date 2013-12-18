4a9ca0a06581d74b2a7369d3e58c56837707d73c
daniffig <daniffig@gmail.com>
2013-12-18 15:51:52 -0300

    Se terminó el drag and drop.

--- JBomb/src/com/example/jbomb/IngameActivity.java
+++ JBomb/src/com/example/jbomb/IngameActivity.java
@@ -4,28 +4,16 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.DragEvent;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.DragShadowBuilder;
 import android.view.View.OnDragListener;
 import android.view.View.OnTouchListener;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
-import android.support.v4.app.NavUtils;
-import android.text.Layout;
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.content.ClipData;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 
 public class IngameActivity extends Activity {
-	
-	private android.widget.RelativeLayout.LayoutParams layoutParams;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -61,15 +49,17 @@ public class IngameActivity extends Activity {
 		@Override
 		public boolean onTouch(View view, MotionEvent motionEvent) {
 			// TODO Auto-generated method stub
-			if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+			
+			switch (motionEvent.getAction())
+			{
+			case MotionEvent.ACTION_DOWN:
 				ClipData data = ClipData.newPlainText("", "");
 				DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
 				view.startDrag(data, shadowBuilder, view, 0);
-				//view.setVisibility(View.INVISIBLE);
-				return true;
-			} else {
-				return false;
+				break;
 			}
+			
+			return true;
 		}
 	}
 	
@@ -78,20 +68,18 @@ public class IngameActivity extends Activity {
 		@Override
 		public boolean onDrag(View v, DragEvent event) {	
 			
+			View ingameBomb = IngameActivity.this.findViewById(R.id.ingameBombImage);
+			View ingameImage = IngameActivity.this.findViewById(v.getId());
+						
 			switch (event.getAction())
 			{
-			case DragEvent.ACTION_DRAG_ENTERED:
-
-			    Toast.makeText(IngameActivity.this.getApplicationContext(), "Entre", Toast.LENGTH_SHORT).show();		
-				break;
-			
 			case DragEvent.ACTION_DRAG_EXITED:
-			    Toast.makeText(IngameActivity.this.getApplicationContext(), "Sali", Toast.LENGTH_SHORT).show();	
+				ingameBomb.setVisibility(View.VISIBLE);
 				break;
-				
+
 			case DragEvent.ACTION_DROP:
-			    Toast.makeText(IngameActivity.this.getApplicationContext(), "Solte", Toast.LENGTH_SHORT).show();				
-			break;
+				IngameActivity.this.openQuiz(ingameImage);				
+				break;
 			}
 		    
 			return true;
